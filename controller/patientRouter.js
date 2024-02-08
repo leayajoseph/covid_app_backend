0ba6586eba487b00ae09f823822cca352b3ea188
leayajoseph <leaya231@gmail.com>
2024-02-08 16:23:54 +0530

    view added

--- controller/patientRouter.js
+++ controller/patientRouter.js
@@ -13,6 +13,12 @@ router.post("/patient_entry",async(req,res)=>{
     )
 })
 
+    router.get("/patient_view",async(req,res)=>{
+        let data=await patientModel.find()
+        res.json(data)
+    })
+
+
 
 
 
